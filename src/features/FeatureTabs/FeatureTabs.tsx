2b7163487fce5e495004d494016189c8d5cf502c
Kunal Ninawe <ninawekunal@gmail.com>
2026-04-18 19:15:48 -0700

    Clean up design for feature tabs

--- src/features/FeatureTabs/FeatureTabs.tsx
+++ src/features/FeatureTabs/FeatureTabs.tsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
 import features from "./data/features.json";
 import { Link } from "react-router-dom";
-import { Button, Chip, Container, Grid, Stack, Tab, Tabs } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Chip,
+  Container,
+  Divider,
+  Grid,
+  Stack,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
 
 export default function FeatureTabs() {
   const [selected, setSelected] = useState(0);
@@ -11,30 +24,36 @@ export default function FeatureTabs() {
   }
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="sm" sx={{ gap: 1 }}>
       <Tabs value={selected} onChange={handleChange} variant="scrollable">
         {features.map((feature, idx) => {
           return <Tab key={idx} label={feature.name} />;
         })}
       </Tabs>
 
-      <Grid direction="row" container spacing={2}>
-        <Container maxWidth="sm">
-          <h2>{features[selected].name}</h2>
-        </Container>
-        <Container>
-          <Stack direction="row" sx={{ gap: 1, flexWrap: "wrap" }}>
-            {features[selected].skills.map((skill, index) => {
-              return <Chip label={skill.title} key={index} clickable />;
-            })}
-          </Stack>
-        </Container>
-        <Container>
-          <Link to={features[selected].demoUrl}>
-            <Button variant="outlined">Go to Live Demo</Button>
-          </Link>
-        </Container>
-      </Grid>
+      <Card raised>
+        <CardHeader
+          title={features[selected].name}
+          action={
+            <Link to={features[selected].demoUrl}>
+              <Button variant="outlined">Go to Live Demo</Button>
+            </Link>
+          }
+          subheader={features[selected].subHeading}
+        />
+        <Divider />
+        <CardContent>
+          <Container>
+            <Typography variant="h6">Skills / Learnings</Typography>
+            <Stack direction="row" sx={{ gap: 1, flexWrap: "wrap" }}>
+              {features[selected].skills.map((skill, index) => {
+                return <Chip label={skill.title} key={index} clickable />;
+              })}
+            </Stack>
+          </Container>
+        </CardContent>
+        <Container></Container>
+      </Card>
     </Container>
   );
 }
